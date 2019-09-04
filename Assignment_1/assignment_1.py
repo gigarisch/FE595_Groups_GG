4168eaad3216c925e1c3d4fd6a12e9c359e68cd9
gigarisch <gigarisch@gmail.com>
2019-09-04 11:08:45 -0400

    Sine and Cosine plot
    Added plot of sine and cosine to the same plot.

---
+++ Assignment_1/assignment_1.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  4 10:37:42 2019
+
+@author: gordon.garisch
+"""
+
+# Import packages
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Create values for x axis
+X = np.linspace(0, 2*np.pi, 360)
+
+# Create plots
+plt.cla()
+plt.plot(X, np.cos(X))                  #Plot for cosine
+plt.plot(X, np.sin(X))                  #Plot for sine
+plt.legend(('cosine', 'sine'))          #Add legend
+plt.xlabel('Angle in Rad')              #Add x label
+plt.ylabel('Function Value')            #Add y label
+plt.title('Trigonometric Functions')    #Add title
+plt.show()                              #Show plot
