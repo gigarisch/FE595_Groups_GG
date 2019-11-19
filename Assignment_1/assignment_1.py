d6fc3ceefec65ee03329afd88f3495881e6b034d
gigarisch <gigarisch@gmail.com>
2019-11-19 09:27:13 -0500

    Added __name__ == '__main__' condition

--- Assignment_1/assignment_1.py
+++ Assignment_1/assignment_1.py
@@ -18,17 +18,21 @@ Description:
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Create values for x axis
-X = np.linspace(0, 2*np.pi, 360)
+def trig_plot():
+    # Create values for x axis
+    X = np.linspace(0, 2*np.pi, 360)
+    
+    # Create plots
+    plt.cla()                               #Remove existing plots
+    plt.plot(X, np.cos(X))                  #Plot for cosine
+    plt.plot(X, np.sin(X))                  #Plot for sine
+    plt.plot(X, np.tan(X))                  #BC: Added tan plot
+    plt.legend(('cosine', 'sine', 'tangent'))   #Add legend #BC: modified legend
+    plt.xlabel('Angle in Rad')              #Add x label
+    plt.ylabel('Function Value')            #Add y label
+    plt.title('Trigonometric Functions')    #Add title
+    plt.ylim(-3, 3)                         #BC: set limits to maintain view of exisitng plots
+    plt.show()                              #Show plot
 
-# Create plots
-plt.cla()                               #Remove existing plots
-plt.plot(X, np.cos(X))                  #Plot for cosine
-plt.plot(X, np.sin(X))                  #Plot for sine
-plt.plot(X, np.tan(X))                  #BC: Added tan plot
-plt.legend(('cosine', 'sine', 'tangent'))   #Add legend #BC: modified legend
-plt.xlabel('Angle in Rad')              #Add x label
-plt.ylabel('Function Value')            #Add y label
-plt.title('Trigonometric Functions')    #Add title
-plt.ylim(-3, 3)                         #BC: set limits to maintain view of exisitng plots
-plt.show()                              #Show plot
+if __name__ == "__main__":
+    trig_plot()
